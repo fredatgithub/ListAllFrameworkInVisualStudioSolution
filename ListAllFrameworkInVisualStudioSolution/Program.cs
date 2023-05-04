18b781efa551ee9421d68be2515dd6d9fee88231
fred <domaine75@gmail.com>
2023-05-04 20:02:33 +0200

    Ajoutez des fichiers projet.

---
+++ ListAllFrameworkInVisualStudioSolution/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAllFrameworkInVisualStudioSolution
+{
+  internal class Program
+  {
+    static void Main(string[] arguments)
+    {
+      // findstr /s "TargetFrameworkVersion" *.csproj
+      // cmd in visual studio solution directory under DOS
+      Action<string> Display = Console.WriteLine;
+      Display("List all framework versions in a Visual Studio Solution");
+      var solutionPath = ".";
+      var projectSuffix = "csproj,vbproj";
+      if (arguments.Length > 0)
+      {
+        solutionPath = arguments[0];
+      }
+
+      Display(solutionPath);
+      // get all csproj and vbproj
+
+
+
+      Display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
---
+++ ListAllFrameworkInVisualStudioSolution/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("ListAllFrameworkInVisualStudioSolution")]
+[assembly: AssemblyDescription("List All Framework versions In a Visual Studio Solution")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Freddy Juhel")]
+[assembly: AssemblyProduct("ListAllFrameworkInVisualStudioSolution")]
+[assembly: AssemblyCopyright("Copyright ©  MIT Freddy Juhel 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("152ce498-55f1-4531-aa49-819b72ab6a14")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
