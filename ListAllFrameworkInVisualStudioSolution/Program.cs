cab2da44ddb8c71681c9c5e4994205a400d79000
fred <domaine75@gmail.com>
2023-05-05 00:30:38 +0200

    add code to display csproj framework version

--- ListAllFrameworkInVisualStudioSolution/Program.cs
+++ ListAllFrameworkInVisualStudioSolution/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +16,7 @@ namespace ListAllFrameworkInVisualStudioSolution
       // cmd in visual studio solution directory under DOS
       Action<string> Display = Console.WriteLine;
       Display("List all framework versions in a Visual Studio Solution");
-      var solutionPath = ".";
+      var solutionPath = "..\\..";
       var projectSuffix = "csproj,vbproj";
       if (arguments.Length > 0)
       {
@@ -23,11 +25,47 @@ namespace ListAllFrameworkInVisualStudioSolution
 
       Display(solutionPath);
       // get all csproj and vbproj
+      string pattern = "*.csproj";
+      string keyword = "TargetFrameworkVersion";
+      var result = new List<string>();
+      foreach (var file in Directory.GetFiles(solutionPath, pattern))
+      {
+        result.Add($"{Path.GetFileName(file)} is in Framework {SearchFile(keyword, file).Replace(keyword, "").Replace("<", "").Replace(">", "").Replace("/", "").Replace(" ", "")}");
+      }
 
-
+      foreach (var item in result)
+      {
+        Display(item);
+      }
 
       Display("Press any key to exit:");
       Console.ReadKey();
+    }
+
+    private static string SearchFile(string keyword, string filename)
+    {
+      var result = string.Empty;
+      try
+      {
+        using (var sr = new StreamReader(filename))
+        {
+          var line = string.Empty;
+          do
+          {
+            line = sr.ReadLine();
+            if ( line != null && line.Contains(keyword))
+            {
+              result = line;
+            }
+          }
+          while (!(line is null));
+        }
+      }
+      catch (Exception)
+      {
+      }
+
+      return result;
     }
   }
 }
